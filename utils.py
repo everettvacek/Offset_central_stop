8bc13465f0cb697e31137f357f9cea3c95f5eb38
Everett <everettvacek2020@u.northwestern.edu>
2021-12-02 13:09:37 -0600

    function for nu_b_darkring, moved psf to utils

--- utils.py
+++ utils.py
@@ -1,15 +1,17 @@
 import numpy as np
 from skimage.transform import rescale, SimilarityTransform, warp
 from skimage.morphology import disk
+from scipy.special import jv
 
-def add_center_stop(array, r, shift = 0):
-    # if r<1 use it as percentage of maximum dimension
+def add_center_stop(array, b, shift = 0):
+    '''
+    array: aperture array.
+    b: fractional radius of central stop.
+    shift: shift in pixels from center.
+    '''
     center_stop = np.zeros_like(array)
-    if r<=1:
-        r = np.floor(np.max(array.shape)*r/2).astype(int)
-    else:
-        r = int(r)
-    d = disk(r, dtype=array.dtype)
+    b = np.floor(np.max(array.shape)*b/2).astype(int)
+    d = disk(b, dtype=array.dtype)
     center_stop[center_stop.shape[0]//2 - d.shape[0]//2:center_stop.shape[0]//2 + d.shape[0]//2+1,\
           center_stop.shape[1]//2 - d.shape[1]//2:center_stop.shape[1]//2 + d.shape[1]//2+1] = d
     tform = SimilarityTransform(translation=(-shift, 0))
@@ -18,10 +20,18 @@ def add_center_stop(array, r, shift = 0):
     return array
 
 def create_aperture(aperture_radius, array_size, downscale_factor, b = 0, shift = 0):
+    '''
+    aperture_radius: final aperture radius in pixels (after downscaling).
+    array_size: size of final array after embedding downscaled aperture array.
+    downscale_factor: integer downscaling of aperture_radius before embedding into final array.
+    b: fractional radius of central stop.
+    shift: shift in pixels from center.
+    '''
     # Creat edge smoothed aperture via rescaling high res aperture.
     aperture = disk(int(aperture_radius*downscale_factor), dtype='double')
-    if b > 0:
-        aperture = add_center_stop(aperture, r=b, shift = shift)
+    if b != 0:
+        # add central stop
+        aperture = add_center_stop(aperture, b=b, shift = shift)
     aperture = np.pad(aperture, pad_width = int(aperture_radius*downscale_factor))
     aperture = rescale(aperture, scale = 1/downscale_factor)
     # Insert in to array of zeros.
@@ -29,3 +39,17 @@ def create_aperture(aperture_radius, array_size, downscale_factor, b = 0, shift 
     a[array_size//2-aperture.shape[0]//2:array_size//2+aperture.shape[0]//2,
       array_size//2-aperture.shape[0]//2:array_size//2+aperture.shape[0]//2] = aperture
     return a
+
+def psf(v, b = None):
+    # Bessel function calculation of 1D psf.
+    '''
+    v: (i.e. nu) 1-D array for domain of the psf.
+    b: fractional radius of central stop.
+    '''
+    if b is None or b is 0:
+        # psf of circular aperture
+        return (2*jv(1,v)/v)**2
+    else:
+        # psf of circular aperture with center stop
+        return (1/(1-b**2))**2*\
+            (2*jv(1,v)/v-b**2*2*jv(1,v*b)/(v*b))**2
